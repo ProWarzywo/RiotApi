3dd23a58744f05b557272d00e14ebe388b4f996c
Jakub Wawro <67758751+ProWarzywo@users.noreply.github.com>
2021-12-18 15:00:33 +0100

    add wins/losses/winrate

--- riotapp/src/App.js
+++ riotapp/src/App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import PlayerGUI from './PlayerGUI'
 import './App.css'
 function App(){
     const[searchPlayerName, setSearchName] = useState("");
     const[searchPlayerTag, setSearchTag] = useState("");
-
     const [playerInfo, setPlayerInfo] = useState("")
-    const[playerMatchId, setPlayerMatchId] = useState([])
     const [playerData, setPlayerData] = useState({})
-    const [playerHistory,setPlayerHistory] = useState({})
     const [playerLeague, setPlayerLeague] = useState({})
-    const [playerTier,setPlayerTier] = useState("")
-    const [GameInfo, setGameInfo] = useState({});
+    // const [playerHistory,setPlayerHistory] = useState({})
+    // const[playerMatchId, setPlayerMatchId] = useState([])
+    // const [GameInfo, setGameInfo] = useState({});
     // const [profileIcon, setProfileIcon] = useState['']
+    const ApiKey = "RGAPI-ae629e73-f9ad-404c-aec7-5c8b89da5222";
+        
         function searchForPlayer(event){
-            const ApiKey = "RGAPI-a6876056-0f9c-43d5-bf98-dc112b3f8b60";
+   
             axios.get(`/riot/account/v1/accounts/by-riot-id/${searchPlayerName}/${searchPlayerTag}?api_key=${ApiKey}`)
             .then(function (response){
                 setPlayerData(response.data.puuid)
@@ -24,15 +24,9 @@ function App(){
             axios.get(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${searchPlayerName}?api_key=${ApiKey}`)
             .then(function(response){
                 setPlayerInfo(response.data)
-                console.log(playerInfo.id)
+
             })
-            axios.get(`https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/${playerInfo.id}?api_key=${ApiKey}`)
-            .then(function(response){
-                setPlayerLeague(response.data[0])
-               console.log(playerLeague)
-               setPlayerTier(response.data[0].tier)
-            
-            })
+           
             // axios.get(`/lol/match/v5/matches/by-puuid/${playerData}/ids?start=20&count=20&api_key=${ApiKey}`)
             // .then(function(response){
             //     setPlayerMatchId(response.data[0])
@@ -49,14 +43,26 @@ function App(){
             // console.log(GameInfo)
             // })
         }
-      
-
+        function SearchPlayerLeague(){
+            axios.get(`https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/${playerInfo.id}?api_key=${ApiKey}`)
+            .then(function(response){
+                setPlayerLeague(response.data[1])
+                console.log(playerLeague)
+                
+            })
+        }
+        async function test(){
+            await searchForPlayer()
+            await SearchPlayerLeague()
+        }
+    
+        
         return(
             <> 
                 <input type="text" name="gameName"  onChange={e => setSearchName(e.target.value)}/> 
                 <input type="text" name="gameTag"  onChange={e => setSearchTag(e.target.value)}/> 
-                <button onClick={searchForPlayer}>Search</button>  
-                <PlayerGUI name={searchPlayerName} icon={playerInfo.profileIconId} level={playerInfo.summonerLevel} tier={playerTier} rank={playerLeague.rank}/>
+                <button onClick={test}>Search</button>  
+                <PlayerGUI name={searchPlayerName} icon={playerInfo.profileIconId} level={playerInfo.summonerLevel} tier={playerLeague.tier} rank={playerLeague.rank} wins={playerLeague.wins} losses={playerLeague.losses}/>
             </>
         );
     }
--- riotapp/src/PlayerGUI.js
+++ riotapp/src/PlayerGUI.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import './PlayerGUI.css';
-import BRONZE from './Images/Emblem_Bronze.png';
-import SILVER from './Images/Emblem_Silver.png';
-
-import PLATINUM from './Images/Emblem_Platinum.png';
-import DIAMOND from './Images/Emblem_Diamond.png';
-import MASTER from './Images/Emblem_Master.png';
-import GRANDMASTER from './Images/Emblem_Grandmaster.png';
-import CHALLENGER from './Images/Emblem_Challenger.png';
-
+import GOLD from './Images/Emblem_GOLD.png'
 function PlayerGUI(props){
  
     return(
         <div className="playerGUI">
             {props.name}
             <img src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/profileicon/${props.icon}.png`} ></img>
-            {props.level}
-            <img src={`./Images/Emblem_GOLD.png`} alt={props.tier} />
+            {props.level != undefined ? props.level + " level" : null}
+
+            <div className="sideBlock">
+                <div className="rankBox">
+                    <img src={`/static/media/Emblem_${props.tier}.617f139e.png`} alt={props.tier} className="RankImg"/> 
+                </div>
+                <div className="RankInfo">
+                    {props.tier != undefined && props.rank != undefined ? props.tier + " " +props.rank : null} <br />
+                    <span>
+                        {props.wins != undefined ? "Wins: " + props.wins : null}
+                        <br />
+                        {props.losses != undefined ?"Losses: " + props.losses : null}
+                        <br />
+                    </span>
+                    <span>
+                        {props.wins != undefined && props.losses != undefined ? "Winrate: " + ((props.wins/(props.wins + props.losses)) * 100).toFixed(1) + "%" : null}
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
